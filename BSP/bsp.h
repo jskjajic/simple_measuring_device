498eed31446750bf1bb2aadc32bb3d9e6d8b2397
Yao Jinda <2023302091013@whu.edu.cn>
2026-05-22 23:15:21 +0800

    Create bsp.h
    头文件

---
+++ BSP/bsp.h
@@ -0,0 +1,18 @@
+#ifndef BSP_H
+#define BSP_H
+
+/******************系统头文件***************/
+#include <ti/devices/msp/msp.h>
+#include <ti/driverlib/driverlib.h>
+#include <ti/driverlib/m0p/dl_core.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "math.h"
+#include "ti_msp_dl_config.h"
+/*******************************************/
+/******************用户自定义头文件*********/
+#include "oled.h"
+/*******************************************/
+#endif
